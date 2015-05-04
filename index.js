a5bfff91f85c5a4386aa75a202869048296a6a5c
Daria <dashka.kh@gmail.com>
2015-05-04 12:50:33 -0700

    node view as part of the library

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var graph = require('./lib/graph.js');
+var graph = require('./lib/graph.js'),
+    nodeView = require('./lib/nodeView.js');
 
 module.exports = {
-  Graph: graph
+  Graph: graph,
+  NodeView: nodeView.NodeView,
+  Rect: nodeView.Rect
 };
